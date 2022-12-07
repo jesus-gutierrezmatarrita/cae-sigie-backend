1c0cd0ad1870dff7a7cc5ddae91911ae10435a21
Jesús Gutiérrez Matarrita <jesusantonio.gutierrezmatarrita@gmail.com>
2022-12-07 17:50:44 -0600

    Added CORS policy, added missing getters and setters

--- src/main/java/cr/ac/ucr/paraiso/analisis/caesigiebackend/entity/TipoDeActividad.java
+++ src/main/java/cr/ac/ucr/paraiso/analisis/caesigiebackend/entity/TipoDeActividad.java
@@ -10,4 +10,23 @@ public class TipoDeActividad {
     private int idTipo;
     @Column(name = "nombreTipo", unique = false, length = 50, nullable = false)
     private String nombreTipo;
+
+    public int getIdTipo() {
+        return idTipo;
+    }
+    public void setIdTipo(int idTipo) {
+        this.idTipo = idTipo;
+    }
+    public String getNombreTipo() {
+        return nombreTipo;
+    }
+    public void setNombreTipo(String nombreTipo) {
+        this.nombreTipo = nombreTipo;
+    }
+    @Override
+    public String toString() {
+        return "TipoDeActividad [idTipo=" + idTipo + ", nombreTipo=" + nombreTipo + "]";
+    }
+
+    
 }
