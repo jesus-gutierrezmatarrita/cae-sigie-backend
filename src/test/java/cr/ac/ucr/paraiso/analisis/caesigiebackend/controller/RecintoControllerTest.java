a6e04bf0703f81d4b2c3998cef7c3c631cb825e1
YerlinLeal <yerlin.leal@ucr.ac.cr>
2022-12-08 09:20:59 -0600

    Actualizacion en ConsultaActividadAcademica

--- src/test/java/cr/ac/ucr/paraiso/analisis/caesigiebackend/controller/RecintoControllerTest.java
+++ src/test/java/cr/ac/ucr/paraiso/analisis/caesigiebackend/controller/RecintoControllerTest.java
@@ -31,64 +31,66 @@ import cr.ac.ucr.paraiso.analisis.caesigiebackend.service.RecintoService;
 @WebMvcTest(value = RecintoController.class)
 class RecintoControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @MockBean
-    private RecintoService service;
+        @MockBean
+        private RecintoService service;
 
-    private ArrayList<Recinto> recintos;
-    ArrayList<ConsultaActividadAcademica> consultasActividadesAcademicas;
-    ArrayList<PersonaGraduada> personasGraduadas;
-    Recinto recinto;
+        private ArrayList<Recinto> recintos;
+        ArrayList<ConsultaActividadAcademica> consultasActividadesAcademicas;
+        ArrayList<PersonaGraduada> personasGraduadas;
+        Recinto recinto;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-        recintos = new ArrayList<Recinto>();
-        consultasActividadesAcademicas = new ArrayList<ConsultaActividadAcademica>();
-        personasGraduadas = new ArrayList<PersonaGraduada>();
-        recinto = new Recinto(1, "30", "Recinto de Paraiso, Sede del Atlantico", 2511 - 0000, "Cartago", "Paraiso",
-                "Paraiso", "Paraiso de Cartago, del parque de Paraiso, un kilometro y medio carretera a Orosi",
-                consultasActividadesAcademicas, personasGraduadas);
-        recintos.add(recinto);
-    }
+        @BeforeEach
+        void setUp() {
+                MockitoAnnotations.initMocks(this);
+                recintos = new ArrayList<Recinto>();
+                consultasActividadesAcademicas = new ArrayList<ConsultaActividadAcademica>();
+                personasGraduadas = new ArrayList<PersonaGraduada>();
+                recinto = new Recinto(1, "30", "Recinto de Paraiso, Sede del Atlantico", 2511 - 0000, "Cartago",
+                                "Paraiso",
+                                "Paraiso",
+                                "Paraiso de Cartago, del parque de Paraiso, un kilometro y medio carretera a Orosi",
+                                consultasActividadesAcademicas, personasGraduadas);
+                recintos.add(recinto);
+        }
 
-    @Test
-    void obtenerRecintos_DatosSonValidos_ListaRecintos() throws Exception {
-        Mockito.when(service.obtenerRecintos()).thenReturn(recintos);
+        @Test
+        void obtenerRecintos_DatosSonValidos_ListaRecintos() throws Exception {
+                Mockito.when(service.obtenerRecintos()).thenReturn(recintos);
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-                "/recinto").accept(
-                        MediaType.APPLICATION_JSON);
+                RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+                                "/recinto").accept(
+                                                MediaType.APPLICATION_JSON);
 
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+                MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 
-        System.out.println(result.getResponse());
-        String expected = "[{\"idRecinto\":1,\"codigoRecinto\":\"30\",\"nombre\":\"Recinto de Paraiso, Sede del Atlantico\",\"contacto\":2511,\"provincia\":\"Cartago\",\"canton\":\"Paraiso\",\"distrito\":\"Paraiso\",\"direccion\":\"Paraiso de Cartago, del parque de Paraiso, un kilometro y medio carretera a Orosi\",\"consultas\":[],\"personasGraduadas\":[]}]";
+                System.out.println(result.getResponse());
+                String expected = "[{\"idRecinto\":1,\"codigoRecinto\":\"30\",\"nombre\":\"Recinto de Paraiso, Sede del Atlantico\",\"contacto\":2511,\"provincia\":\"Cartago\",\"canton\":\"Paraiso\",\"distrito\":\"Paraiso\",\"direccion\":\"Paraiso de Cartago, del parque de Paraiso, un kilometro y medio carretera a Orosi\",\"consultas\":[],\"personasGraduadas\":[]}]";
 
-        JSONAssert.assertEquals(expected, result.getResponse()
-                .getContentAsString(), false);
+                JSONAssert.assertEquals(expected, result.getResponse()
+                                .getContentAsString(), false);
 
-    }
+        }
 
-    @Test
-    void guardarRecinto_InsercionEsCorrecta_Recinto() throws Exception {
-        Mockito.when(service.guardarRecinto(Mockito.any(Recinto.class))).thenReturn(recinto);
+        @Test
+        void guardarRecinto_InsercionEsCorrecta_Recinto() throws Exception {
+                Mockito.when(service.guardarRecinto(Mockito.any(Recinto.class))).thenReturn(recinto);
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .post("/recinto")
-                .accept(MediaType.APPLICATION_JSON)
-                .content(
-                        "{\"idRecinto\":1,\"codigoRecinto\":\"30\",\"nombre\":\"Recinto de Paraiso, Sede del Atlantico\",\"contacto\":2511,\"provincia\":\"Cartago\",\"canton\":\"Paraiso\",\"distrito\":\"Paraiso\",\"direccion\":\"Paraiso de Cartago, del parque de Paraiso, un kilometro y medio carretera a Orosi\",\"consultas\":[],\"personasGraduadas\":[]}")
-                .contentType(MediaType.APPLICATION_JSON);
+                RequestBuilder requestBuilder = MockMvcRequestBuilders
+                                .post("/recinto")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .content(
+                                                "{\"idRecinto\":1,\"codigoRecinto\":\"30\",\"nombre\":\"Recinto de Paraiso, Sede del Atlantico\",\"contacto\":2511,\"provincia\":\"Cartago\",\"canton\":\"Paraiso\",\"distrito\":\"Paraiso\",\"direccion\":\"Paraiso de Cartago, del parque de Paraiso, un kilometro y medio carretera a Orosi\",\"consultas\":[],\"personasGraduadas\":[]}")
+                                .contentType(MediaType.APPLICATION_JSON);
 
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+                MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 
-        System.out.println(result.getResponse());
-        MockHttpServletResponse response = result.getResponse();
+                System.out.println(result.getResponse());
+                MockHttpServletResponse response = result.getResponse();
 
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
-    }
-    
+                assertEquals(HttpStatus.OK.value(), response.getStatus());
+        }
+
 }
